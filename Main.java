896f94ad3558683083d1bf2c5bbf22274b8cd9f6
aura <joshuaura7822@gmail.com>
2025-05-28 22:09:43 +0300

    deletion of arrays --> java

--- Main.java
+++ Main.java
@@ -1,14 +1,21 @@
 public class Main{
     public static void main(String[]args){
         int arr[] = new int[3];
-        System.out.println("Array before insertion : ");
-        for(int i=0; i<3; i++)
-            System.out.println("arr["+i+"] = "+ arr[i]);
-                               // which index and pulling element at the index
-        System.out.println("Inserting array elements...");
-        for(int i=0; i<3; i++) {
-            arr[i] = i + 3;
-            System.out.println("arr[" + i + "] = " + arr[i]);
+        int n = arr.length;
+        System.out.println("Arrays before deletion : ");
+        for(int i=0; i<3; i++){
+            arr[i]= i+3;
+            System.out.println("Arr["+i+"] ="+arr[i]);
         }
+        System.out.println("Deleting...");
+        for(int i=1; i<n; i++){
+            arr[i]=i+1;
+            n=n-1;
+        }
+        System.out.println("Arrays after deletion : ");
+        for(int i=0; i<3; i++){
+            System.out.println("arr["+i+"] ="+arr[i]);
+        }
+
     }
 }
