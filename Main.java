1c7ff5b99251aef58d0be573e8e9bbf747fb48a0
aura <joshuaura7822@gmail.com>
2025-05-28 20:34:17 +0300

    insertion of arrays -->  data structures

--- Main.java
+++ Main.java
@@ -1,21 +1,14 @@
-import java.util.Scanner;
 public class Main{
     public static void main(String[]args){
-        Scanner aura = new Scanner(System.in);
-        System.out.println("Enter rank : ");
-        int rank;
-        rank =  aura.nextInt();
-        switch (rank){
-            case 1:
-                System.out.println("Amount is 1000000");
-                break;
-            case 2:
-                System.out.println("Amount is 500000");
-                break;
-            case 3:
-                System.out.println("Amount is 250000");
-            default:
-                System.out.print("Amount is 0");
+        int arr[] = new int[3];
+        System.out.println("Array before insertion : ");
+        for(int i=0; i<3; i++)
+            System.out.println("arr["+i+"] = "+ arr[i]);
+                               // which index and pulling element at the index
+        System.out.println("Inserting array elements...");
+        for(int i=0; i<3; i++) {
+            arr[i] = i + 3;
+            System.out.println("arr[" + i + "] = " + arr[i]);
         }
     }
 }
