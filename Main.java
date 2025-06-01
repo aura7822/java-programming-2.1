255db749ed31bc13b251a78ec9cabce8a89d8dfc
aura <joshuaura7822@gmail.com>
2025-06-01 21:48:47 +0300

    For printing area of a triangle

--- Main.java
+++ Main.java
@@ -1,21 +1,18 @@
+import java.util.Scanner;
 public class Main{
-    public static void main(String[]args){
-        int arr[] = new int[3];
-        int n = arr.length;
-        System.out.println("Arrays before deletion : ");
-        for(int i=0; i<3; i++){
-            arr[i]= i+3;
-            System.out.println("Arr["+i+"] ="+arr[i]);
-        }
-        System.out.println("Deleting...");
-        for(int i=1; i<n; i++){
-            arr[i]=i+1;
-            n=n-1;
-        }
-        System.out.println("Arrays after deletion : ");
-        for(int i=0; i<3; i++){
-            System.out.println("arr["+i+"] ="+arr[i]);
-        }
+    double base, height, ans;
+    void calculate(){
+        Scanner aura = new Scanner(System.in);
+        System.out.println("Enter Base : ");
+        base = aura.nextDouble();
+        System.out.println("Enter Height : ");
+        height = aura.nextDouble();
+        ans = 0.5*base*height;
+        System.out.printf("Area = %.2f", ans);
 
+    }
+    public static void main(String[]args){
+        Main shape = new Main();
+        shape.calculate();
     }
 }
